89518f96d0ae89bf44f73b99dd6e5a44df650498
Rinnelis <gorgoramodus@gmail.com>
2020-06-13 17:41:57 +0300

    Добавляет PropTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
+
+const filmTitles = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {filmTitle, filmGenre, releaseDate} = props;
+  const {filmTitle, filmGenre, releaseDate, titles} = props;
 
   return (
     <Main
       filmTitle={filmTitle}
       filmGenre={filmGenre}
       releaseDate={releaseDate}
+      titles={titles}
     />
   );
+};
+
+App.propTypes = {
+  filmTitle: PropTypes.string.isRequired,
+  filmGenre: PropTypes.string.isRequired,
+  releaseDate: PropTypes.number.isRequired,
+  titles: PropTypes.oneOf(filmTitles),
 };
 
 export default App;
